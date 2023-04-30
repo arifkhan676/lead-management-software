3ee85b03e0f85ee8e9f6480ca92c35f12592f3ab
arifkhan676 <2014751049@uits.edu.bd>
2023-05-01 00:09:34 +0600

    dropdown

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,34 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./dash.css"
+import logo from "../logo-light.png"
+/* cd lead-management-software */
+import ListIcon from '@mui/icons-material/List';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { DropDownList } from "@progress/kendo-react-dropdowns";
 
 function Dashboard(){
+    const list = useState(['All Leads','New Leads','Import Leads']);
+    const [keep,allKeep] = React.useState('My Leads');
+    const handleChange=(event)=>{
+         allKeep(event.target.value);
+    }
     return(
     <div className="f-div">
         <div className="left-div">
            <div className="title-div">
             <div className="reob">
-                <h1>reobiz</h1>
+               <img src={logo} alt="logo" />
             </div>
             <div className="admin-div">
             <h2>Admin</h2>
             </div>
            </div>
            <div className="left-content">
-               <div className="each-cont">
-                <h2>Dashboard</h2>
-               </div>
-               <div className="each-cont">
-                <h2>My leads</h2>
-               </div>
-               <div className="each-cont">
-                <h2>Campaign</h2>
-               </div>
-               <div className="each-cont">
-                <h2>Counselor</h2>
-               </div>
-               <div className="each-cont">
-                <h2>Profile</h2>
-               </div>
+               <li className='list'> <a href="#"> <ListIcon className='ListI' /> <h2>Dashboard</h2>    <ArrowForwardIosIcon className='ListI' />       </a>  </li>
+               <li className='list' > <a href="#"  > <ListIcon className='ListI' /> <DropDownList data={list} onChange={handleChange} value={keep} />   <ArrowForwardIosIcon className='ListI' />      </a>    </li>
+               <li className='list'> <a href="#"> <ListIcon className='ListI' /> <h2>Campaign</h2>    <ArrowForwardIosIcon className='ListI' />       </a> </li>
+               <li className='list'> <a href="#"> <ListIcon className='ListI' /> <h2>Counselor</h2>   <ArrowForwardIosIcon className='ListI' />       </a> </li>
+               <li className='list'> <a href="#"> <ListIcon className='ListI' /> <h2>Profile</h2>     <ArrowForwardIosIcon className='ListI' />     </a>  </li>
+               <li className='list'> <a href="#"> <ListIcon className='ListI' /> <h2>Setting</h2>      <ArrowForwardIosIcon className='ListI' />     </a>    </li>
+
            </div>
         </div>
         <div className="right-div">
